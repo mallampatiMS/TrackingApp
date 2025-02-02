60e927a928e3100b1562a43b4e7efcd097ee6b40
Mallampati Srinivas <mallampati.it@gmail.com>
2025-02-03 02:36:36 +0800

    return changes done at config file

---
+++ src/main/java/com/tracking/config/RateLimiterConfig.java
@@ -0,0 +1,29 @@
+package com.tracking.config;
+
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.time.Duration;
+
+//@Configuration
+//public class RateLimiterConfiguration {
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public RateLimiter rateLimiter() {
+        //  RateLimiterConfig config = RateLimiterConfig.custom()
+        //         .timeoutDuration(Duration.ofMillis(500)) // Timeout when acquiring a permission
+        //         .limitRefreshPeriod(Duration.ofSeconds(1)) // Refresh rate
+        //         .limitForPeriod(10) // Max requests per period
+        //         .build();
+
+        return RateLimiter.of("trackingApiRateLimiter", config -> config
+                .timeoutDuration(Duration.ofMillis(500)) // Timeout when acquiring a permission
+                .limitRefreshPeriod(Duration.ofSeconds(1)) // Refresh rate
+                .limitForPeriod(10) // Max requests per period
+                );
+        //return RateLimiter.of("trackingRateLimiter", () -> config);  //  Use a Supplier        
+    }
+}
